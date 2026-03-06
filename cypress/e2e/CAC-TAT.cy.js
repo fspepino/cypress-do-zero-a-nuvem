e82a9f9ce01a47a5ba2587394e27bba90b026e16
Your Name <you@example.com>
2026-03-06 12:29:55 -0300

    inicial

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,16 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    // root-level hook
+    cy.visit('./src/index.html')
+  })  
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+  it('preenche os campos obrigatórios e envia o formulário', ()=>{
+    cy.get('[name="firstName"]').type('nome')
+    cy.get('[name="lastName"]').type('sobrenome')
+    cy.get('[name="phone"').type('519999999')
+    cy.get('[name="teste')
+    
+  })
+})
